ad84fd449b5ca52a5e157d43535dfe6a2b3662c3
bit-delta <148971548+bit-delta@users.noreply.github.com>
2023-10-26 11:09:38 -0400

    load at 1-1, use loop from pyboy docs

--- pyBoyTest.py
+++ pyBoyTest.py
@@ -4,8 +4,13 @@ gb_path = './DK.gb'
 
 pyboy = PyBoy(gb_path)
 assert pyboy.cartridge_title() == "DONKEY KONG"
+pyboy.set_emulation_speed(1)
 
-# state = open('DK.gb.state', 'rb')
-# pyboy.load_state(state)
-while True:
-    pyboy.tick()
+state = open('DK-1-1.gb.state', 'rb')
+pyboy.load_state(state)
+while not pyboy.tick():
+    pass
+
+# save_file = open('DK-1-1.gb.state', 'wb')
+# pyboy.save_state(save_file)
+pyboy.stop(save=False)
